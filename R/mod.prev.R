d38dbbedd6c50f1f117e31ed4b92a35368dec3da
Samuel Jenness <sjenness@uw.edu>
2015-10-17 10:19:33 -0700

    Remove unneeded vectors from prevalence module

--- R/mod.prev.R
+++ R/mod.prev.R
@@ -21,18 +21,16 @@ prevalence.hiv <- function(dat, at) {
 
   # Initialize vectors
   if (at == 1) {
+    dat$epi$i.num <- rNA
+    dat$epi$num <- rNA
 
-    # Prev vectors
-    dat$epi$s.num <-  dat$epi$i.num <- rNA
-    dat$epi$num <- dat$epi$cumlNum <- rNA
-    dat$epi$cumlInc <- dat$epi$incr <- rNA
+    dat$epi$i.num.male <- rNA
+    dat$epi$i.num.feml <- rNA
+    dat$epi$i.prev.male <- rNA
+    dat$epi$i.prev.feml <- rNA
 
-    dat$epi$s.num.male <- dat$epi$s.num.feml <- rNA
-    dat$epi$i.num.male <- dat$epi$i.num.feml <- rNA
-    dat$epi$i.prev.male <- dat$epi$i.prev.feml <- rNA
-    dat$epi$incr.male <- dat$epi$incr.feml <- rNA
-
-    dat$epi$num.male <- dat$epi$num.feml <- rNA
+    dat$epi$num.male <- rNA
+    dat$epi$num.feml <- rNA
     dat$epi$meanAge <- rNA
     dat$epi$propMale <- rNA
 
@@ -42,36 +40,20 @@ prevalence.hiv <- function(dat, at) {
 
     dat$epi$b.flow <- rNA
     dat$epi$ds.flow <- dat$epi$di.flow <- rNA
-
   }
 
-  ### Prevalence ###
-
-  ## Overall prevalence/incidence
-  dat$epi$s.num[at] <- sum(active == 1 & status == 0, na.rm = TRUE)
   dat$epi$i.num[at] <- sum(active == 1 & status == 1, na.rm = TRUE)
   dat$epi$num[at] <- sum(active == 1, na.rm = TRUE)
-  dat$epi$cumlNum[at] <- dat$epi$num[1] + sum(dat$epi$b.flow, na.rm = TRUE)
-  dat$epi$cumlInc[at] <- sum(dat$epi$si.flow, na.rm = TRUE)
-  dat$epi$incr[at] <- (dat$epi$si.flow[at] / dat$epi$s.num[at])*5200
 
-  ## Sex-specific prevalence
-  dat$epi$s.num.male[at] <- sum(active == 1 & status == 0 & male == 1, na.rm = TRUE)
-  dat$epi$s.num.feml[at] <- sum(active == 1 & status == 0 & male == 0, na.rm = TRUE)
   dat$epi$i.num.male[at] <- sum(active == 1 & status == 1 & male == 1, na.rm = TRUE)
   dat$epi$i.num.feml[at] <- sum(active == 1 & status == 1 & male == 0, na.rm = TRUE)
   dat$epi$i.prev.male[at] <- sum(active == 1 & status == 1 & male == 1, na.rm = TRUE) /
                              sum(active == 1 & male == 1, na.rm = TRUE)
   dat$epi$i.prev.feml[at] <- sum(active == 1 & status == 1 & male == 0, na.rm = TRUE) /
                              sum(active == 1 & male == 0, na.rm = TRUE)
-  dat$epi$incr.male[at] <- (dat$epi$si.flow.male[at] / dat$epi$s.num.male[at])*5200
-  dat$epi$incr.feml[at] <- (dat$epi$si.flow.feml[at] / dat$epi$s.num.feml[at])*5200
 
-  ### Demographics ###
   dat$epi$num.male[at] <- sum(active == 1 & male == 1, na.rm = TRUE)
   dat$epi$num.feml[at] <- sum(active == 1 & male == 0, na.rm = TRUE)
-
-  ## Age
   dat$epi$meanAge[at] <- mean(age[active == 1], na.rm = TRUE)
   dat$epi$propMale[at] <- mean(male[active == 1], na.rm = TRUE)
 
